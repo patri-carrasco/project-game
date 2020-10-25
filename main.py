abf11a058967533b2a443f9c48ab27d1fddb75ba
Patricia <patri.carrasco@gmail.com>
2020-10-25 19:07:32 +0100

    Funciona, casi perfecto

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
 import src.functions as fn
 
-words = ["list", "tuple","comprenhensions"]
-
-print("Hi, wellcome  to hagnman game's")
-char_n = input("Enter a char ")
-
-# get the char in lowercase
-char = char_n.lower()
+#words = ["list", "tuple","comprenhensions"]
+words = ["list", "moco"]
 
 #get random word from a list
 word = fn.random_word(words)
 print(word)
+
+print("Hi, wellcome  to hagnman game's")
+
+
+#game(word)
+
+
+right = []
+wrong = []
+
+
+board = fn.get_empty_word(len(word))
+#game
+
+
+
+while len(wrong)<len(fn.hangman) and '-' in board:
+  #check if char is alphabet
+  char_n = input("Enter a char ")
+
+  # get the char in lowercase
+  char = char_n.lower()
+  fn.print_game_status(wrong,right,board)
+
+  if  not fn.is_letter(char_n):
+    char_n = input("Enter a correct char ")
+  
+  
+
+  pos_char_word = []
+
+  if fn.pos_char(char,word)!= []:
+    pos_char_word = fn.pos_char(char,word)
+    
+    right.append(char)
+    print(right)
+    
+    for i in pos_char_word:
+      board[i] = char
+    
+   
+  else:
+    wrong.append(char)
+  
+
+  
+  
+  
+
+
+
+
+
+
+
+
+
 
 
 
--- src/functions.py
+++ src/functions.py
@@ -2,41 +2,145 @@ import random
 
 
 
+
+def is_letter(char):
+  '''
+  This function chekc the char is in alphabet
+      arg:
+          char
+      return 
+          letter : True or False
+  '''
+  alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  letter = False
+  if char in alphabet:
+      letter = True
+
+  return (letter)
+
 def random_word(word):
-  #This function returns a random word from a list
-  # arg:
-    # list word
-  # return
-    # word
+  '''
+      This function returns a random word from a list
+        arg:
+            word
+        return
+              random word 
+  '''
   random_w =random.choice(word)
   return (random_w)
-  
-  def is_letter(char):
+
+def pos_char(char,word):
     '''
-    This function chekc the char is in alphabet
-        arg:
-            char
-        return 
-            letter : True or False
+        This function looks for the character in the word and returns the positions where they appear 
+            args :
+                char
+                word
+            return
+                positions
     '''
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    letter = False
-    if char in alphabet:
-        letter = True
- 
-    return (letter)
-    
+    pos = []
+    for i in enumerate(word):
+        if i[1] == char:
+            pos.append(i[0])
+
+    return (pos)
+
+
+def get_empty_word(size):
+  arr = []
+  for i in range(size):
+    arr += '-'
+  return arr
+
+def print_game_status(wrong, right,board):
+  print(hangman[len(wrong)-1])
+  print(board)
+  print(f'Mistake char {wrong}')
+  print(f'Right {right}')
+
 hangman = [
-  '''+---+
-     |   |
-     |
-     |
-     |
-     |
-     |
-     |
-     
-     --------
-     --------
-  '''
+'''
++---+
+|   |
+|
+|
+|
+|
+|
+|
+--------
+--------
+''',
+'''
++---+
+|   |
+|   O
+|   
+| 
+|
+|
+|
+--------
+--------
+''',
+'''
++---+
+|   |
+|   O
+|   |
+|   |
+|   |
+|
+|
+--------
+--------
+''',
+'''
++---+
+|   |
+|   O
+|  /|
+|   |   
+|   
+|
+|
+--------
+--------
+''',
+'''
++---+
+|   |
+|   O
+|  /|\
+|   |
+|   
+|
+|
+--------
+--------
+''',
+'''
++---+
+|   |
+|   O
+|  /|\
+|   |
+|  /
+|
+|
+--------
+--------
+''',
+'''
++---+
+|   |
+|   O
+|  /|\
+|   |
+|  / \
+|
+|
+--------
+--------
+'''
 ]
