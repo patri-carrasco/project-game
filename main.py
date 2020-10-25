a79cf2f457854ee9d313032fad3285a6b63d2c9c
Patricia <patri.carrasco@gmail.com>
2020-10-25 10:08:35 +0100

    Create is_letter

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-#import functions 
+import src.functions as fn
+
+words = ["list", "tuple","comprenhensions"]
 
 print("Hi, wellcome  to hagnman game's")
-char = input("Enter a char ")
+char_n = input("Enter a char ")
+
+# get the char in lowercase
+char = char_n.lower()
+
+#get random word from a list
+word = fn.random_word(words)
+print(word)
+
+
+
--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,6 @@
 import random
 
-words = ["list", "tuple","comprenhensions"]
+
 
 def random_word(word):
   #This function returns a random word from a list
@@ -11,6 +11,21 @@ def random_word(word):
   random_w =random.choice(word)
   return (random_w)
   
+  def is_letter(char):
+    '''
+    This function chekc the char is in alphabet
+        arg:
+            char
+        return 
+            letter : True or False
+    '''
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    letter = False
+    if char in alphabet:
+        letter = True
+ 
+    return (letter)
+    
 hangman = [
   '''+---+
      |   |
